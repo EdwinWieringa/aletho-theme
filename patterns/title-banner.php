db48d00f432f7330dc2283cda68332c9ab4a91b3
Edwin Wieringa <edwieringa@gmail.com>
2025-07-23 11:02:11 +0200

    first FSE-theme commit

---
+++ patterns/title-banner.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Title: Title Banner
+ * Slug: aletho/title-banner
+ * Categories: features
+ */
+?>
+
+<!-- wp:cover {"overlayColor":"primary-blue","isUserOverlayColor":true,"style":{"border":{"top":{"color":"var:preset|color|secundary-light-blue","width":"1px"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"width":"0px","style":"none"}},"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover" style="border-top-color:var(--wp--preset--color--secundary-light-blue);border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;margin-top:0;margin-bottom:0"><span aria-hidden="true" class="wp-block-cover__background has-primary-blue-background-color has-background-dim-100 has-background-dim"></span>
+    <div class="wp-block-cover__inner-container"><!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+        <figure class="wp-block-image size-large"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/logo-aletho-wit.png')); ?>" alt="" /></figure>
+        <!-- /wp:image -->
+    </div>
+</div>
+<!-- /wp:cover -->
