320055f9af28a0c7d37fde464de146874f498079
Edwin Wieringa <edwieringa@gmail.com>
2025-07-23 15:27:08 +0200

    Title-gradient pattern added

---
+++ patterns/title-gradient.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Title: Title Banner
+ * Slug: aletho/title-gradient
+ * Categories: features
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"gradient":"aletho-light-blue-gradient","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-aletho-light-blue-gradient-gradient-background has-background" style="margin-top:0;margin-bottom:0"><!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:post-title {"level":1,"style":{"spacing":{"margin":{"bottom":"0"},"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"fontSize":"xxl"} /--></div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
