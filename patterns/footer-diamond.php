26c066d1b59f02c750e872dba698a872a7122057
Edwin Wieringa <edwieringa@gmail.com>
2025-08-06 15:32:50 +0200

    Forgot the image

---
+++ patterns/footer-diamond.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Title: Footer Diamond
+ * Slug: aletho/footer-diamond
+ * Categories: features
+ */
+?>
+
+<!-- wp:image {"id":317,"sizeSlug":"full","linkDestination":"none","className":"footer-diamond"} -->
+<figure class="wp-block-image size-full footer-diamond">
+    <img src="<?php echo esc_url(get_theme_file_uri('assets/images/diamond.png')); ?>" alt="" class="wp-image-317" />
+</figure>
+<!-- /wp:image -->
