91a9991db09b6c5a2692616e8ba18cead4c3f519
Edwin Wieringa <edwieringa@gmail.com>
2026-07-13 22:37:30 +0200

    Switched some blocks around for easier styling

--- patterns/title-gradient.php
+++ patterns/title-gradient.php
@@ -7,8 +7,8 @@
  */
 ?>
 
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"gradient":"aletho-light-blue-gradient","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-aletho-light-blue-gradient-gradient-background has-background" style="margin-top:0;margin-bottom:0">
+<!-- wp:group {"className":"title-gradient","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"gradient":"aletho-light-blue-gradient","layout":{"type":"constrained"}} -->
+<div class="wp-block-group title-gradient has-aletho-light-blue-gradient-gradient-background has-background" style="margin-top:0;margin-bottom:0">
     <!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
     <div class="wp-block-group" style="margin-top:0;margin-bottom:0">
         <!-- wp:post-title {"level":1,"style":{"spacing":{"margin":{"bottom":"0"},"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"fontSize":"xxl"} /-->
